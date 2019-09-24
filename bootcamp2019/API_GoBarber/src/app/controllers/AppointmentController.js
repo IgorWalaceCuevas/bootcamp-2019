4dcf3eec7fff17b2b42913f738fd852d1535b5b8
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2019-09-23 23:18:59 -0300

    Listando agendamento do usuário

--- bootcamp2019/API_GoBarber/src/app/controllers/AppointmentController.js
+++ bootcamp2019/API_GoBarber/src/app/controllers/AppointmentController.js
@@ -1,8 +1,38 @@
-import User from '../models/User';
-import Appointment from '../models/Appointment';
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import User from '../models/User';
+import File from '../models/File';
+import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      order: ['date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -27,6 +57,28 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    //check for past dates
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    //check date availability
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({
+        error: 'Appointment date is not available',
+      });
     }
 
     const appointment = await Appointment.create({
