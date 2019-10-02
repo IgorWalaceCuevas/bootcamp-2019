8e7da82398bf13c3eee95a06de3da3c1cf1fd8a7
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2019-10-02 00:46:22 -0300

    Configuração processo background e nodemailer

---
+++ bootcamp2019/API_GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4dc2920180a869',
+    pass: '461f73a0220d0b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
