9ddcb32154041784002fe4d5a5f952340638ff7d
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2019-12-21 17:22:51 -0300

    fineshed

--- bootcamp2019/projeto-react/src/pages/Repository/index.js
+++ bootcamp2019/projeto-react/src/pages/Repository/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import { Loading, Owner, IssueList } from './styles';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -14,10 +16,9 @@ export default class Repository extends Component {
   };
 
   state = {
-    repository: {
-      issues: [],
-      loading: true,
-    },
+    repository: {},
+    issues: [],
+    loading: true,
   };
   async componentDidMount() {
     const { match } = this.props;
@@ -39,12 +40,37 @@ export default class Repository extends Component {
       repository: repository.data,
       issues: issues.data,
     });
-
-    console.log(repository);
-    console.log(issues);
   }
   render() {
     const { repository, issues, loading } = this.state;
-    return <h1>Repository</h1>;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return (
+      <Container>
+        <Owner>
+          <Link to={'/'}>Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
