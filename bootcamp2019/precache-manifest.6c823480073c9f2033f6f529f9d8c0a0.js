f235b67a55bb80cb8b65d982d51e2f63d45a23f8
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2020-01-05 15:52:46 -0300

    publish test

---
+++ bootcamp2019/precache-manifest.6c823480073c9f2033f6f529f9d8c0a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7792f54079503eef411d464d7761c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c28f74604da0493767c",
+    "url": "/static/js/2.5d6f8a35.chunk.js"
+  },
+  {
+    "revision": "21fea1ec54fab0ebb0f2",
+    "url": "/static/js/main.a7960214.chunk.js"
+  },
+  {
+    "revision": "a39ea18bbd8dd32dd6eb",
+    "url": "/static/js/runtime-main.c104fd6a.js"
+  }
+]);
